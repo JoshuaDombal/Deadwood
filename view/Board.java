1b91b44f2ebd3afeee1fd7fa8ce227d216ae4c72
carpend3 <carpenterdylan@comcast.net>
2017-05-24 23:20:54 -0700

    Add files via upload

--- view/Board.java
+++ view/Board.java
@@ -8,18 +8,28 @@ import javax.imageio.ImageIO;
 public class Board extends JLayeredPane {
     private JLabel boardLabel;
 
-    public Board() throws Exception {
+    public Board(model.Board model) throws Exception {
 
         boardLabel = new JLabel();
 
         Class cls = getClass();
-        cls.getResourceAsStream("board.jpg");
-
-
         ImageIcon icon = new ImageIcon(ImageIO.read(cls.getResourceAsStream("board.jpg")));
-
         boardLabel.setIcon(icon);
         add(boardLabel, new Integer(0));
         boardLabel.setBounds(0, 0, icon.getIconWidth(), icon.getIconHeight());
+
+        ArrayList<model.Set> sets = model.getSets();
+
+        Role r;
+        for(Set s : sets){
+            model.Role[] R = model.getRoles();
+
+            for(model.Role r : R){
+                r = new Role(R.getX(),R.getY(),R.getH(),R.getW(),R);
+                add(r, new Integer(3));
+            }
+        }
+
+
     }
 }
