d901474c9cf593e5ecf62886061001c32eec1419
carpend3 <carpenterdylan@comcast.net>
2017-05-24 22:42:57 -0700

    Add files via upload

---
+++ view/Role.java
@@ -0,0 +1,47 @@
+package view;
+
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import javax.imageio.ImageIO;
+
+public class Role
+    extends javax.swing.JLayeredPane
+    implements model.Role.Listener
+{
+
+    private JLabel dice;
+
+    public Role(int x, int y, int h, int w, model.Role r){
+        setBounds(x,y,h,w);
+
+        dice = new JLabel();
+        dice.setVisible(false);
+        add(dice, new Integer(0));
+        dice.setBounds(0,0,h,w);
+
+        r.subscribe(this);
+        changed(r);
+    }
+
+    private static ImageIcon getIcon(Player player){
+        String file = (player.getColor() + player.getRank() + ".png");
+
+        ImageIcon icon = null;
+        try{
+            Class cls = Role.class;
+            icon = new ImageIcon(ImageIO.read(cls.getResourcesAsStream(file)));
+        }catch (Exception e){
+            e.printStackTrace();
+            System.exit(1);
+        }
+        return icon;
+    }
+
+    public void changed (model.Role r){
+        if(r.checkForPlayer()){
+            dice.setIcon(getIcon(r.getPlayer()));
+        }
+
+        dice.setVisible(r.checkForPlayer());
+    }
+}
